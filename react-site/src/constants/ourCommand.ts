5cf12281aa9e8ade0bc3e7671a42a076f442a916
Dmitriy Vasilev <raoffonom@icloud.com>
2023-03-01 11:55:51 +0700

    Fix for text

--- react-site/src/constants/ourCommand.ts
+++ react-site/src/constants/ourCommand.ts
@@ -1,37 +1,37 @@
 import Andrey from 'img/command-avatars/Andrey.jpg';
 import Dmitry from 'img/command-avatars/Dmitry.jpg';
-import DrSpider from 'img/command-avatars/DrSpider.jpg';
+// import DrSpider from 'img/command-avatars/DrSpider.jpg';
 import Geya from 'img/command-avatars/Geya.jpg';
 import Kaya from 'img/command-avatars/Kaya.jpg';
 
 export const ourCommand = [
   {
-    name: 'Дмитрий',
-    role: 'Creator / Ceo',
+    name: 'Dmitriy Vasilev',
+    role: 'Founder / CTO',
     img: Dmitry,
     instLink: 'https://instagram.com/reactnativeru',
   },
   {
-    name: 'Gaya',
-    role: 'Muse / Soul',
+    name: 'Gaya Kamskaya',
+    role: 'CEO',
     img: Geya,
     instLink: 'https://instagram.com/playom',
   },
   {
-    name: 'Andrey',
-    role: 'Coding',
-    img: Andrey,
-    instLink: 'https://instagram.com/aeroluxx',
-  },
-  {
-    name: 'Kaya',
+    name: 'Kaya Ko',
     role: 'SMM',
     img: Kaya,
     instLink: 'https://instagram.com/kayaa_ko',
   },
   {
-    name: 'Dr. Spider',
-    role: 'Manager',
-    img: DrSpider,
+    name: 'Ozerkov Andrey',
+    role: 'Scrum Master',
+    img: Andrey,
+    instLink: 'https://instagram.com/aeroluxx',
   },
+  // {
+  //   name: 'Dr. Spider',
+  //   role: 'Manager',
+  //   img: DrSpider,
+  // },
 ];
