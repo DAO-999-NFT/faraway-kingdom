ae51d499ca0ce1959fe80e85324d92ca75185f2e
Danil <rosesmaster2007@gmail.com>
2023-04-01 21:06:57 +0500

    gallerysection fix

--- react-site/src/components/sections/Gallery.tsx
+++ react-site/src/components/sections/Gallery.tsx
@@ -10,7 +10,7 @@ export function GallerySection() {
         <div className="content-container py-[1.5em] self-center">
           <SimpleImageSlider
             width={950}
-            height={504}
+            height={600}
             showBullets={false}
             showNavs={true}
             images={imagesData}
