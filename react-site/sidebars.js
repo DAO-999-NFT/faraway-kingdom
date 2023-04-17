cbdb40ccecdd575dad4030b72954b42d61a834e1
Dmitriy Vasilev <raoffonom@icloud.com>
2023-04-17 16:50:24 +0700

    Start init

--- react-site/sidebars.js
+++ react-site/sidebars.js
@@ -2,6 +2,7 @@
 module.exports = {
   someSidebar: {
     ['Intro']: ['intro'],
+    ['Start']: ['start/1', 'start/2'],
     ['Ambassadors']: ['ambassadors/1', 'ambassadors/2'],
     ['Investors']: ['investors/1'],
   },
