e40012c0132ebd596975865a4302a51e3095a998
Dmitriy Vasilev <raoffonom@icloud.com>
2023-04-18 15:57:19 +0700

    New articles

--- react-site/sidebars.js
+++ react-site/sidebars.js
@@ -3,6 +3,7 @@ module.exports = {
   someSidebar: {
     ['Intro']: ['intro'],
     ['Start']: ['start/1', 'start/2'],
+    ['Web3']: ['web3/1', 'web3/2'],
     ['Ambassadors']: ['ambassadors/1', 'ambassadors/2'],
     ['Investors']: ['investors/1'],
   },
