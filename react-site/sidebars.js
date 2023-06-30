3ff52d528eda57533b03457ccc6833e63aa1d599
Navernoss <chuklinovivan@yandex.com>
2023-06-30 14:31:35 +0500

    Add article Lens Protocol

--- react-site/sidebars.js
+++ react-site/sidebars.js
@@ -4,7 +4,7 @@ module.exports = {
     ['Intro']: ['intro'],
     ['Investors']: ['investors/1'],
     ['Start']: ['start/1', 'start/2'],
-    ['Web3']: ['web3/1', 'web3/2'],
+    ['Web3']: ['web3/1', 'web3/2', 'web3/3'],
     ['Ambassadors']: ['ambassadors/1', 'ambassadors/2'],
     ['Links']: ['Links'],
   },
